4ce6b95f6da487d3b12326db628b166b482b9a1f
Danny Zhao <Danny.H.Zhao@gmail.com>
2024-08-09 15:34:21 -0700

    product-service done

---
+++ product-service/src/main/java/com/techie/microservice/product_service/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.techie.microservice.product_service.repository;
+
+import com.techie.microservice.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
